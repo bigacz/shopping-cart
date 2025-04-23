ee72d271d3c30e504effa036e34caaadd3cd03da
bigacz <sbigacz@gmail.com>
2025-04-23 18:48:02 +0200

    Add navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,20 @@
+import { Link } from 'react-router';
 import './Navbar.module.css';
+import { ShoppingCart } from 'lucide-react';
 
 function Navbar() {
-  return;
+  return (
+    <nav>
+      <h1>
+        <a href="/">Kitchen</a>
+      </h1>
+      <a href="/shop">Products</a>
+      <a href="/cart">
+        <ShoppingCart />
+        Cart
+      </a>
+    </nav>
+  );
 }
 
 export default Navbar;
---
+++ src/components/Navbar/Navbar.test.jsx
@@ -0,0 +1,36 @@
+import Navbar from './Navbar';
+
+import { expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { createRoutesStub } from 'react-router';
+
+const Stub = createRoutesStub([
+  {
+    path: '/',
+    Component: Navbar,
+  },
+]);
+
+it('has header that redirects to root directory', () => {
+  render(<Stub />);
+
+  const heading = screen.getByRole('heading');
+  const link = screen.getByRole('link', { name: /kitchen/i });
+
+  expect(heading).toContainElement(link);
+  expect(link).toHaveAttribute('href', '/');
+});
+
+it('has link that redirects to shop', () => {
+  render(<Stub />);
+
+  const link = screen.getByRole('link', { name: /products/i });
+  expect(link).toHaveAttribute('href', '/shop');
+});
+
+it('has link that redirects to cart', () => {
+  render(<Stub />);
+
+  const link = screen.getByRole('link', { name: /cart/i });
+  expect(link).toHaveAttribute('href', '/cart');
+});
