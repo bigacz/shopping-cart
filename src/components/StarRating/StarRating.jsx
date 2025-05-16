fa759d5597589e528a40d2b4c5fb83605e5f5a11
bigacz <sbigacz@gmail.com>
2025-05-16 19:56:19 +0200

    Add StarRating

---
+++ src/components/StarRating/StarRating.jsx
@@ -0,0 +1,29 @@
+import { Star } from 'lucide-react';
+import './StarRating.module.css';
+
+function StarRating({ rating }) {
+  const stars = [];
+
+  let filledAmount = 5;
+  if (!Number.isInteger(rating)) {
+    filledAmount = 5;
+  } else if (rating < 1) {
+    filledAmount = 1;
+  } else if (rating > 5) {
+    filledAmount = 5;
+  } else {
+    filledAmount = rating;
+  }
+
+  for (let i = 0; i < filledAmount; i++) {
+    stars.push(<Star key={i} fill="currentColor" data-testid="star-filled" />);
+  }
+
+  while (stars.length < 5) {
+    stars.push(<Star data-testid="star-empty" />);
+  }
+
+  return <div>{stars}</div>;
+}
+
+export default StarRating;
---
+++ src/components/StarRating/StarRating.test.jsx
@@ -0,0 +1,41 @@
+import { render, screen } from '@testing-library/react';
+import { expect, it } from 'vitest';
+import StarRating from './StarRating';
+
+it.each([
+  { rating: 1, filled: 1 },
+  { rating: 2, filled: 2 },
+  { rating: 3, filled: 3 },
+  { rating: 4, filled: 4 },
+  { rating: 5, filled: 5 },
+  { rating: undefined, filled: 5 },
+  { rating: 6, filled: 5 },
+  { rating: 0, filled: 1 },
+])(
+  'displays $filled filled stars when rating is $rating',
+  ({ rating, filled }) => {
+    render(<StarRating rating={rating} />);
+
+    const filledStars = screen.getAllByTestId('star-filled');
+    expect(filledStars.length).toBe(filled);
+  }
+);
+
+it.each([
+  { rating: 1, empty: 4 },
+  { rating: 2, empty: 3 },
+  { rating: 3, empty: 2 },
+  { rating: 4, empty: 1 },
+  { rating: 5, empty: 0 },
+  { rating: undefined, empty: 0 },
+  { rating: 6, empty: 0 },
+  { rating: 0, empty: 4 },
+])(
+  'displays $empty empty stars when rating is $rating',
+  ({ rating, empty }) => {
+    render(<StarRating rating={rating} />);
+
+    const emptyStars = screen.queryAllByTestId('star-empty');
+    expect(emptyStars.length).toBe(empty);
+  }
+);
