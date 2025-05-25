395775fdf6d2481ca880f67f522125e4197c8338
bigacz <sbigacz@gmail.com>
2025-05-25 16:59:03 +0200

    Add shop page

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,7 +1,22 @@
+import { useOutletContext } from 'react-router';
 import './Shop.module.css';
 
 function Shop() {
-  return;
+  const { products } = useOutletContext();
+
+  return (
+    <div>
+      {products.map(({ id, title, price, images }) => {
+        return (
+          <div key={id}>
+            <img src={images[0]} alt={title} />
+            <span>{title}</span>
+            <span>{price}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Shop;
---
+++ src/pages/Shop/Shop.test.jsx
@@ -0,0 +1,67 @@
+import { expect, it } from 'vitest';
+import Shop from './Shop';
+import { createRoutesStub, Outlet } from 'react-router';
+import { render, screen } from '@testing-library/react';
+
+const products = [
+  {
+    id: 48,
+    title: 'product name 1',
+    price: 7.99,
+    images: ['https://placehold.co/700x500'],
+  },
+  {
+    id: 52,
+    title: 'product name 2',
+    price: 90.99,
+    images: ['https://placehold.co/600x400'],
+  },
+];
+
+const Stub = createRoutesStub([
+  {
+    path: '/',
+    Component: () => <Outlet context={{ products }} />,
+    children: [
+      {
+        path: 'shop',
+        Component: Shop,
+      },
+    ],
+  },
+]);
+
+it('renders product image', () => {
+  render(<Stub initialEntries={['/shop']} />);
+
+  const image = screen.getByAltText('product name 1');
+  expect(image).toHaveAttribute('src', 'https://placehold.co/700x500');
+});
+
+it('renders product name', () => {
+  render(<Stub initialEntries={['/shop']} />);
+
+  screen.getByText('product name 1');
+});
+
+it('renders product price', () => {
+  render(<Stub initialEntries={['/shop']} />);
+
+  screen.getByText('7.99');
+});
+
+it('renders multiple products', () => {
+  render(<Stub initialEntries={['/shop']} />);
+
+  const image1 = screen.getByAltText('product name 1');
+  expect(image1).toHaveAttribute('src', 'https://placehold.co/700x500');
+
+  screen.getByText('product name 1');
+  screen.getByText('7.99');
+
+  const image2 = screen.getByAltText('product name 2');
+  expect(image2).toHaveAttribute('src', 'https://placehold.co/600x400');
+
+  screen.getByText('product name 2');
+  screen.getByText('90.99');
+});
