32ef735cfc957388e88d7001caaa8ab048b2c688
bigacz <sbigacz@gmail.com>
2025-05-31 15:00:33 +0200

    Add products fetching

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,29 @@
 import Navbar from 'components/Navbar/Navbar';
 import Footer from 'components/Footer/Footer';
 import { Outlet } from 'react-router';
+import { useEffect, useState } from 'react';
+
+const apiUrl = 'https://dummyjson.com/products/category/kitchen-accessories';
 
 function App() {
+  const [products, setProducts] = useState();
+  const [cart, setCart] = useState({});
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const request = await fetch(apiUrl);
+      const json = await request.json();
+
+      setProducts(json);
+    }
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <Navbar />
-      <Outlet />
+      <Outlet context={{ products }} />
       <Footer />
     </>
   );
--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -1,26 +1,54 @@
-import { it, vi } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { beforeEach, expect, it, vi } from 'vitest';
+import { act, render, screen } from '@testing-library/react';
 
 import App from './App';
 import Navbar from 'components/Navbar/Navbar';
 import Footer from 'components/Footer/Footer';
+import { Outlet } from 'react-router';
 
-vi.mock('components/Navbar/Navbar', () => ({
-  default: () => <div data-testid="navbar" />,
-}));
+global.fetch = vi.fn(() =>
+  Promise.resolve({ json: () => Promise.resolve('fetchTestValue') })
+);
+
+vi.mock('react-router', () => {
+  const originalModule = vi.importActual('react-router');
+
+  return {
+    ...originalModule,
+    Outlet: vi.fn(),
+  };
+});
 
 vi.mock('components/Footer/Footer', () => ({
   default: () => <div data-testid="footer" />,
 }));
 
-it('renders navbar', () => {
-  render(<App />);
+vi.mock('components/Navbar/Navbar', () => ({
+  default: () => <div data-testid="navbar" />,
+}));
+
+it('renders navbar', async () => {
+  await act(async () => {
+    render(<App />);
+  });
 
   screen.getByTestId('navbar');
 });
 
-it('renders footer', () => {
-  render(<App />);
+it('renders footer', async () => {
+  await act(async () => {
+    render(<App />);
+  });
 
   screen.getByTestId('footer');
+});
+
+it('renders outlet with products in context', async () => {
+  await act(async () => {
+    render(<App />);
+  });
+
+  expect(Outlet.mock.lastCall[0].context.products).toBe('fetchTestValue');
+
+  vi.restoreAllMocks();
 });
