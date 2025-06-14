5d68af6fbf6ae87beef4455ba759daff78e35dde
bigacz <sbigacz@gmail.com>
2025-06-14 23:10:22 +0200

    Add Cart rendering items

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,7 +1,21 @@
+import { useOutletContext } from 'react-router';
 import './Cart.module.css';
 
 function Cart() {
-  return;
+  const { products, cart } = useOutletContext();
+
+  return (
+    <main>
+      {products.map((product) => (
+        <div key={product.id}>
+          <img src={product.images[0]} alt={product.title} />
+          <span>{cart[product.id]}</span>
+          <span>{product.title}</span>
+          <span>{product.price} €</span>
+        </div>
+      ))}
+    </main>
+  );
 }
 
 export default Cart;
---
+++ src/pages/Cart/Cart.test.jsx
@@ -0,0 +1,124 @@
+import userEvent from '@testing-library/user-event';
+import Cart from './Cart';
+
+import { render, screen } from '@testing-library/react';
+import { Component } from 'lucide-react';
+import { createRoutesStub, Outlet } from 'react-router';
+import { expect, it, vi } from 'vitest';
+
+const products = [
+  {
+    id: 48,
+    title: 'product name one',
+    price: 7.99,
+    images: ['https://placehold.co/600x400'],
+  },
+  {
+    id: 52,
+    title: 'product name two',
+    price: 90.99,
+    images: ['https://placehold.co/700x500'],
+  },
+];
+
+const addProduct = vi.fn();
+const removeProduct = vi.fn();
+
+function setup() {
+  const user = userEvent.setup();
+
+  const cart = {
+    48: 2,
+  };
+
+  const Stub = createRoutesStub([
+    {
+      path: '/',
+      Component: () => (
+        <Outlet context={{ products, cart, addProduct, removeProduct }} />
+      ),
+      children: [
+        {
+          path: 'cart',
+          Component: Cart,
+        },
+      ],
+    },
+  ]);
+
+  render(<Stub initialEntries={['/cart']} />);
+
+  return { user };
+}
+
+function setupWithTwoProducts() {
+  const user = userEvent.setup();
+
+  const cart = {
+    48: 2,
+    52: 1,
+  };
+
+  const Stub = createRoutesStub([
+    {
+      path: '/',
+      Component: () => (
+        <Outlet context={{ products, cart, addProduct, removeProduct }} />
+      ),
+
+      children: [
+        {
+          path: 'cart',
+          Component: Cart,
+        },
+      ],
+    },
+  ]);
+
+  render(<Stub initialEntries={['/cart']} />);
+
+  return { user };
+}
+
+it('renders product name ', () => {
+  setup();
+
+  screen.getByText(/product name one/i);
+});
+
+it('renders product price ', () => {
+  setup();
+
+  screen.getByText(/7.99 €/i);
+});
+
+it('renders product amount ', () => {
+  setup();
+
+  screen.getByText(/2/i);
+});
+
+it('renders product image', () => {
+  setup();
+
+  const image = screen.getByAltText(/product name one/i);
+  expect(image).toHaveAttribute('src', 'https://placehold.co/600x400');
+});
+
+it('renders a button', () => {});
+
+it('renders two products', () => {
+  setupWithTwoProducts();
+
+  screen.getByText(/product name one/i);
+  screen.getByText(/7.99 €/i);
+  screen.getByText(/2/i);
+  const image1 = screen.getByAltText(/product name one/i);
+  expect(image1).toHaveAttribute('src', 'https://placehold.co/600x400');
+
+  screen.getByText(/product name two/i);
+  screen.getByText(/90.99 €/i);
+  screen.getByText(/1/i);
+  const image2 = screen.getByAltText(/product name two/i);
+  expect(image2).toHaveAttribute('src', 'https://placehold.co/700x500');
+});
